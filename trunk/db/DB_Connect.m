33c845f0271f14ad6a086af76c40e09452272f9f
daniel.stolzberg@gmail.com <daniel.stolzberg@gmail.com@ea1da25e-d87d-b06a-1ad7-8ee46c313a2c>
2013-01-04 22:15:34 +0000

    Began to build database functions and guis.  Finally compiled MYM for 64-bit Win 7.  May need to recompile on new systems along with windows 64 bit C++ SDK (see Compiling mym for 64 bit win 7.txt).

---
+++ trunk/db/DB_Connect.m
@@ -0,0 +1,56 @@
+function dbs = DB_Connect(host,user,pass)
+% dbs = DB_Connect
+% dbs = DB_Connect(host,user,pass)
+% dbs = DB_Connect(forcePrompt)
+%
+% Connect to database via mym
+%
+% Returns list of databases if successful connection is established
+%
+% DJS 2013
+
+prevcon = getpref('DB_Connect',{'host','user','pass'},{'','',''});
+if nargin == 1
+    % force update
+    newcon = PromptNewInfo(prevcon);
+    if ~isempty(newcon)
+        host = newcon{1}; user = newcon{2}; pass = newcon{3};
+        mym('open',host,user,pass);
+    end
+    
+elseif nargin == 0
+    % use previous connection parameters
+    host = prevcon{1}; user = prevcon{2}; pass = prevcon{3};
+    
+    try
+        mym('open',prevcon{1},prevcon{2},prevcon{3});
+    catch %#ok<CTCH>
+        newcon = PromptNewInfo(prevcon);
+        if ~isempty(newcon)
+            host = newcon{1}; user = newcon{2}; pass = newcon{3};
+            mym('open',host,user,pass);
+        end
+    end
+    
+end
+
+if ~myisopen
+    error('Unable to connect to database')
+end
+
+dbs = dblist;
+% remove reserved database names
+reserved = {'information_schema','class_lists','db_util','mysql'};
+i = ismember(dbs,reserved);
+dbs(i) = [];
+
+setpref('DB_Connect',{'host','user','pass'},{host,user,pass});
+
+
+function newcon = PromptNewInfo(prevcon)
+prompt  = {'Host','User name','Password'};
+name    = 'Connect to DB';
+opt.Resize      = 'on';
+opt.WindowStyle = 'modal';
+opt.Interpreter = 'none';
+newcon = inputdlg(prompt,name,1,prevcon,opt);
