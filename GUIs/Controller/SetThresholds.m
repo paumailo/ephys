c1dca145a8e9d065a8879f690c2d3446ae40c54a
daniel.stolzberg <daniel.stolzberg@ea1da25e-d87d-b06a-1ad7-8ee46c313a2c>
2013-03-24 03:39:54 +0000

    Fixed issue with Preview function stopping soon after being started.  Added warning if threshold file not found.

--- GUIs/Controller/SetThresholds.m
+++ GUIs/Controller/SetThresholds.m
@@ -15,36 +15,40 @@ if ~exist('fn','var') || isempty(fn)
     fn = 'C:\Electrophys\RunTime Files\Thresh.mat';
 end
 
-load(fn);
-
-for i = 1:length(spike.threshold)
-    tmpName = strcat('Acq.aSnip~',num2str(i));
-    % removed error because it is thrown if too few channels are being run
-    if DA.SetTargetVal(tmpName, spike.threshold(i)) == 0    % set spike detection thresholds
-%                     error('Unable to set threshold');
+if exist(fn,'file')
+    load(fn);
+    
+    
+    for i = 1:length(spike.threshold)
+        tmpName = strcat('Acq.aSnip~',num2str(i));
+        % removed error because it is thrown if too few channels are being run
+        if DA.SetTargetVal(tmpName, spike.threshold(i)) == 0    % set spike detection thresholds
+            %                     error('Unable to set threshold');
+        end
     end
+    
+    % *************************************************************************
+    % NOTE: UNABLE TO GET/SET FILTERS FROM THE SpikePac NeuroFilter COMPONENT.
+    % CONTACT TDT
+    % *************************************************************************
+    % set spike filters
+    % if ~DA.SetTargetVal('Acq.Spike_HP',spike.HP) || ~DA.SetTargetVal('Acq.Spike_LP',spike.LP)
+    %     disp('Unable to set Spike filters!');
+    %     beep
+    % end
+    
+    % set wave (lfp) filters
+    % if ~DA.SetTargetVal('Acq.Wave_HP',wave.HP) || ~DA.SetTargetVal('Acq.Wave_LP',wave.LP)
+    %     disp('Unable to set Wave filters!');
+    %     %         beep
+    % end
+    
+    hoops.spike = spike;
+    hoops.wave  = wave;
+    
+    s = dir(fn);
+    
+    disp(['Hoops set from file created ' datestr(s.date,'mmm.dd,yyyy HH:MM:SS PM')]);
+else
+    warning('Threshold file not found')
 end
-
-% *************************************************************************
-% NOTE: UNABLE TO GET/SET FILTERS FROM THE SpikePac NeuroFilter COMPONENT.
-% CONTACT TDT
-% *************************************************************************
-% set spike filters
-% if ~DA.SetTargetVal('Acq.Spike_HP',spike.HP) || ~DA.SetTargetVal('Acq.Spike_LP',spike.LP)
-%     disp('Unable to set Spike filters!');
-%     beep
-% end
-
-% set wave (lfp) filters
-% if ~DA.SetTargetVal('Acq.Wave_HP',wave.HP) || ~DA.SetTargetVal('Acq.Wave_LP',wave.LP)
-%     disp('Unable to set Wave filters!');
-%     %         beep
-% end
-
-hoops.spike = spike;
-hoops.wave  = wave;
-
-s = dir(fn);
-
-disp(['Hoops set from file created ' datestr(s.date,'mmm.dd,yyyy HH:MM:SS PM')]);
-
