fafc206c4a1ef55564d6356a051f11b81ee2ae5a
daniel.stolzberg <daniel.stolzberg@ea1da25e-d87d-b06a-1ad7-8ee46c313a2c>
2013-07-11 21:06:58 +0000

    SCRATCH_BatchPools2Tank.m runs AutoClass2TDT function on all pooled data in a specific directory

---
+++ trunk/spike_sorting/SCRATCH_BatchPools2Tank.m
@@ -0,0 +1,12 @@
+%% Batch Pools 2 Tanks
+
+% Update directory to tank name and run the script using Ctrl+Enter
+ACdir = 'C:\AutoClass_Files\AC2_RESULTS\TankName';
+
+poolfiles = dir(fullfile(ACdir,'*POOLS.mat'));
+
+POOLS = cellfun(@fullfile,repmat({ACdir},1,length(poolfiles)),{poolfiles.name},'UniformOutput',false);
+
+cellfun(@AutoClass2TDT,POOLS);
+
+
